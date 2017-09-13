f634ddd585ac5af1848edc099a0306ff7e2be246
K_atc <shiftx1026@gmail.com>
2017-09-13 18:23:05 +0900

    Add VFP support patch for S2E

--- Ubuntu_14_04_64bits/build.sh
+++ Ubuntu_14_04_64bits/build.sh
@@ -1,3 +1,19 @@
+count_x_files()
+{
+    COUNT=$(git status --porcelain | grep -v "/patch/" | grep $1 | wc -l)
+    echo $COUNT # return $COUNT
+}
+
 IMAGE="eurecom-s3/avatar-14"
-docker build -t $IMAGE .
+
+docker build -t $IMAGE . || (echo "[!] ERROR: docker build. exit" ; exit)
+echo "[*] DONE: docker build"
+
+if [ $(count_x_files "MM") -ne "0" ]; then
+    echo "[!] There's not staged changes. git add it now"; exit
+fi
+if [ $(count_x_files "??") -ne "0" ]; then
+    echo "[!] There's untracked patch files. git add it now"; exit
+fi
 docker tag $IMAGE $IMAGE:$(git rev-parse --short HEAD)
+echo "[*] DONE: docker tag"
